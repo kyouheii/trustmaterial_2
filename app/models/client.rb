5dcd698a34adff2cd11d94b2c6fe7128da981164
Hitoshi-unten <untenht@gmail.com>
2022-04-06 05:55:57 +0900

    取引先登録空作成ができないようにした

--- app/models/client.rb
+++ app/models/client.rb
@@ -3,5 +3,5 @@ class Client < ApplicationRecord
   has_many :quotations, dependent: :destroy
   has_many :tusin_quotations, dependent: :destroy
   has_many :sanin_quotations, dependent: :destroy
-  validates :client_name,  presence: true, length: { maximum: 50 }
+  validates :client_name, {presence: true, length: { maximum: 50 }}
 end
