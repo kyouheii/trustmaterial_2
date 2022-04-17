99c2e6838a933f39537800d901086e341b5b7a43
Hitoshi-unten <untenht@gmail.com>
2022-04-17 09:14:56 +0900

    取引先登録後画面遷移先変更

--- app/models/sanin_quotation.rb
+++ app/models/sanin_quotation.rb
@@ -1,3 +1,5 @@
 class SaninQuotation < ApplicationRecord
   belongs_to :client, optional: true
+  validates :staff_name, {presence: true, length: { maximum: 50 }}
+  validates :store, {presence: true}
 end
